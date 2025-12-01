033801a2875c740cfc9a540a8ddbd6aa9b311122
cwilson <cwilson@snugzusa.com>
2025-12-01 13:32:53 -0700

    remove background services from api

--- Services/Interfaces/ISesProvisioningRetryService.cs
+++
@@ -1,7 +0,0 @@
-namespace Email.Server.Services.Interfaces
-{
-    public interface ISesProvisioningRetryService
-    {
-        Task RetryFailedProvisionsAsync(CancellationToken cancellationToken = default);
-    }
-}
